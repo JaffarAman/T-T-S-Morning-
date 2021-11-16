f27ec2ea2167705d3b4bbc929a5f60b97bfa0e41
jawantechpk <jawantechpk@gmail.com>
2021-11-16 12:21:09 +0500

    16/11/21

---
+++ class 1/app.js
@@ -0,0 +1,206 @@
+// alert("HELLO CLASS...")
+
+
+
+
+
+// var now = new Date()
+
+// var courseStart = new Date("aug 9 , 2019")
+
+// var msNow = now.getTime()
+// var msCourseStart = courseStart.getTime()
+
+// var msDiff = msNow - msCourseStart
+
+
+// var days = Math.floor( msDiff / (1000 * 60 *60 * 24 * 365))
+
+// // console.log(msNow);
+
+// // console.log(msCourseStart);
+
+// console.log(days)
+
+
+// var now = new Date().toLocaleTimeString()
+
+// console.log(now)
+
+
+// alert("HELLO CLASS");
+
+
+
+// function  function_name  () {
+
+// }   
+
+
+
+// function foo() {
+
+
+//         alert("hello class")
+
+
+// }   
+
+
+///call function//
+// foo()
+// foo()
+// foo()
+
+
+// function kuchBhi () {
+//     console.log("console horha hai....")   
+// }
+
+
+
+// kuchBhi()
+// kuchBhi()
+// kuchBhi()
+// kuchBhi()
+// kuchBhi()
+
+
+
+// function kuchBhi ( name , name1 , name2  ) {
+    
+//     console.log(name , name1 , name2)   
+// }
+
+
+
+// kuchBhi(  "jaffar" , "aman" , "ahmed" )
+
+
+// kuchBhi("ahmed") 
+
+
+// function add(num1 , num2){
+
+//     console.log(num1 + num2)
+
+// }
+
+// var val1 = +prompt("enter value 1")
+// var val2 = +prompt("enter value 2")
+
+
+// add(  val1 , val2  );
+
+
+
+
+
+// function foo(num1 , num2 = 50){
+    
+//     var a = num1 + num2 
+
+//     return a
+
+    
+// }
+
+
+//   var add =  foo(50 , 100)  
+
+// console.log(add)
+
+
+
+
+// function add (val1 , val2 = 10){
+//     return val1 + val2
+// }
+
+
+
+// var addition = add(50 , 80)
+// console.log(addition)
+
+
+
+
+
+// function greet( name ){
+//     alert( "HELLO" + " " + name )
+// }
+
+
+// greet("USAMA")
+
+
+
+// var names  = "Jaffar Aman";
+
+// function greet() {
+
+//     var names  = "Jaffar Aman";
+
+//     console.log(names)
+   
+
+// }
+
+
+
+// greet()
+
+// console.log(names)
+
+
+
+
+// greet()
+
+
+// var myName = "Jaffar"
+
+// function names (){
+//     var myName = "Aman"
+
+//     console.log(myName)
+
+// }
+
+
+
+// names()
+
+
+// var num1 = +prompt("ENTER NUMBER 1")
+// var opt = prompt("ENTER opt")
+// var num2 = +prompt("ENTER NUMBER 2")
+
+
+// function  calc(num1 , opt , num2){
+
+//         if(opt == "+"){
+//             return num1 + num2
+//         }else if(opt === "-"){
+//             return num1 - num2
+//         }else if(opt === "*"){
+//             return num1 * num2
+//         }else if(opt === "/"){
+//             return num1 / num2
+//         }else{
+//             return "enter correct opt"
+//         }
+
+
+// }
+
+// var name = "jaffar"
+
+// var value = calc(num1 , opt , num2)
+
+// document.write(num1 + " " + opt + " " + num2 + " " + "= " +value)
+ 
+
+
+
+
