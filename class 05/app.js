b07017f77874e5cd51f484116821331bb30b4085
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2021-11-25 12:06:19 +0500

    DOM 1st class 25/11/21
    DOM 1st class 25/11/21

---
+++ class 05/app.js
@@ -0,0 +1,91 @@
+// alert("HELLO CLASS")
+
+// var age = 20;
+// if(age === 21){
+//     alert("Allow")
+// }else{
+//     alert("Not Allow")
+
+// }
+
+// switch case
+
+// if ===> case;
+// else ===> default
+
+
+// var age = "20";
+// switch( age ){
+
+//     case "20"  :
+//     console.log("ALLOW")
+//     break;
+
+//     case 22:
+//     console.log("ALLOW")
+//     break
+
+//     default :
+//     console.log("not match")
+//     break
+
+
+// }
+
+
+    /////while loop and do while loop////
+
+//     var userInp = prompt()
+//  for (var index = userInp; index < array.length; index++) {
+//      var element = array[index];
+     
+//  } 
+    
+// var i = 1;
+
+// while( i <=10 ){
+//     ///statement///
+
+//     console.log(i)
+
+//     //increment ///
+//     i++
+
+// }
+
+
+
+// var i = 1
+// do{
+//     console.log(i)
+//     i++
+
+// }while(i > 10)
+
+
+            //the dom////
+
+// var htmlTag = document.childNodes[1]
+// console.log(htmlTag.childNodes)            
+
+
+
+var pappa = document.getElementById("pappa");
+
+// var firstChild = pappa.firstChild
+// console.log(firstChild.previousSibling)
+
+// console.log(pappa.firstChild)
+
+
+// console.log(firstChild.parentNode)
+pappa.firstChild.style.color = "red"
+pappa.lastChild.style.color = "red"
+
+
+var para1 = document.getElementById("para1")
+
+console.log(
+    para1.childNodes[1]
+
+)
