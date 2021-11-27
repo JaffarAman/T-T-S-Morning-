a4cbfcc7e624159a787fcffe8a5271fa8ba667ae
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2021-11-27 11:41:42 +0500

    dom last class

---
+++ class 06/app.js
@@ -0,0 +1,102 @@
+// var a = document.childNodes[1]
+// console.log(a.childNodes[2].childNodes)
+
+
+// var parent = document.getElementById("parent")
+// console.log(parent.childNodes)
+
+// var a = parent.firstChild.nextSibling
+
+// a.style.color = "red"
+// a.innerHTML = "color change"
+
+// console.log(a)
+
+
+// var p = document.getElementsByTagName("p")
+// console.log(p)
+
+
+// p[1].style.color = "red"
+
+
+// var div1 = document.getElementById("div1")
+// console.log(div1)
+// var newPara = div1.getElementsByTagName("p")
+// console.log(newPara)
+
+
+
+// var p1 = document.getElementsByClassName("p1")
+
+// console.log(p1)
+
+
+
+// var div1 = document.getElementById("div1");
+// // console.log(div1.childNodes);
+
+
+// console.log(div1.childNodes.length)
+
+
+
+// var para1 = document.getElementById("para1")
+
+// var checkClassAttr =  para1.hasAttribute("class")
+// // var classValue = para1.getAttribute("class")
+// console.log(checkClassAttr)
+// // console.log(classValue)
+
+// para1.setAttribute("class" , "p2")
+
+
+
+// console.log(para1)
+
+
+// var input1  = document.getElementById("input1");
+
+// console.log(input1.attributes.length)
+
+
+// var h1 = document.createElement("h1")
+// var h1Text = document.createTextNode("HELLO WORLD")
+
+// h1.appendChild(h1Text)
+
+// console.log(h1)
+// // console.log(h1Text)
+
+// var div = document.getElementById("root")
+// var p = document.createElement("p")
+
+// p.innerHTML = "para 1000"
+// p.setAttribute("class" , "p2")
+// // var ptxt = document.createTextNode("para 100")
+// // // console.log(ptxt)
+
+
+// div.appendChild(p)
+// div.appendChild(h1)
+// // p.appendChild(ptxt)
+// console.log(p);
+
+
+
+
+// var parent = document.getElementById("parent");
+// console.log(parent);
+
+
+// // parent.setAttr
+// // parent.className
+
+
+// parent.classList.add("jaffar")
+// parent.classList.add("aman")
+// // parent.classList.remove("aman")
+
+// console.log(parent.classList)
+
+
