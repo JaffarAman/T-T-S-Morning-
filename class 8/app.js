b60dc46e07c37d62989cb76cd80c915bbed59ca9
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2021-12-02 12:30:46 +0500

    12/2/21

---
+++ class 8/app.js
@@ -0,0 +1,122 @@
+// alert("HELLO CLASS...")
+
+
+////password show or hide code//////
+// function showPass(e){
+//     var passInput= document.getElementById("passInput");
+//     console.log(e.className)
+//     if(e.className === "fas fa-lock"){
+//         e.className = "fas fa-unlock" 
+//         passInput.type = "text" 
+//     }else{
+//         e.className = "fas fa-lock"
+//         passInput.type = "password  " 
+
+//     }
+// }
+
+
+//// DARK OR LIGHT MODE... 
+
+// function darkMode(e){
+//     var mainBody = document.getElementById("mainBody")
+//     mainBody.style.backgroundColor = "#040D21"
+
+//     if(e.innerHTML === "DARK MODE"){
+//         e.innerHTML = "LIGHT MODE"
+//         mainBody.style.backgroundColor = "#040D21"
+        
+//     }else{
+//         e.innerHTML = "DARK MODE"
+//         mainBody.style.backgroundColor = "white"
+        
+//     }
+
+// }
+
+
+// change bg color  user value  
+
+// function changeBG(e){
+//     var bgColor = prompt("COLOR NAME....")
+//     var mainBody = document.getElementById("mainBody")
+//     mainBody.style.backgroundColor = bgColor
+// }
+
+
+// FAQS///
+
+// function showA1(e){
+//     console.log(e.nextElementSibling) 
+//     var nextSib = e.nextElementSibling
+//     if(nextSib.className == "a1"){
+//         nextSib.className = "a1Show animate__animated  animate__backInDown"
+
+//     }else{
+//         nextSib.className = "a1"
+
+//     }
+// }   
+
+
+
+
+
+
+
+// function clicks(){
+//     var box = document.getElementById("box1")
+//     console.log(box)
+//     var randowNum = Math.floor( Math.random() * 350 + 2)
+//     var randowNum1 = Math.floor( Math.random() * 350 + 2)
+
+//     console.log(randowNum , randowNum1 )
+//     box.style.margin = randowNum1 + "px" + " " + randowNum + "px"
+// }
+
+
+
+
+// var arr = []
+// console.log(typeof arr)
+
+
+///object in js///
+
+// var arr = ["jaffar" , "aman"]
+
+// var arr = [];
+// arr[0] = "12"
+// console.log(arr[0])
+
+
+// var std1 = {
+
+//     name : "Jaffar" ,
+//     age : 20,
+//     gender : "male" 
+
+// }
+
+// console.log(std1)
+// console.log(std1.age)
+// console.log(std1.gender)
+
+
+// var obj = {}
+
+// obj.names = "Jaffar"
+// console.log(obj)
+
+
+
+// var arr = ["Jaffar", 20 , 80]
+
+// var obj = {
+//     name :" jaffar",
+//     age:20,
+//     englishMArk : 80
+
+// }
+// console.log(typeof arr);
+// console.log(arr);
