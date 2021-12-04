8e9150616aea293e65ba12b46ee26eefb4877ecc
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2021-12-04 17:26:55 +0500

    4/12/21

---
+++ class 09/app.js
@@ -0,0 +1,93 @@
+// function prodModal(price , name , img){
+//     var modalImg = document.getElementById("modalImg")
+//     var modalPrice = document.getElementById("modalPrice")
+//     var modalName = document.getElementById("modalName")
+
+//     modalImg.src = img
+//     modalName.innerHTML = name
+//     modalPrice.innerHTML = price
+
+// }
+
+// name = > property
+
+///behaviour = >method
+
+// var stdName = "Jaffar";
+// var stdAge = 20;
+// var stdClass = "Web & Mobile";
+// var stdRollNum = 101;
+// var stdSection = "Morning";
+
+// []
+
+// var arr = ["talha",21]
+// console.log(arr[1])
+
+// var arr = []
+
+// arr[0] = "Jaffar "
+
+// console.log(arr)
+
+// var obj = {
+//         stdName : "Jaffar"
+// }
+
+// console.log(obj)
+
+// var std1 = {
+
+//     stdName : "Talha",
+//     stdAge : 21
+
+// }
+
+// console.log(std1.stdName)
+// console.log(std1.stdAge)
+
+// var std1 = {
+//   stdName: "Umer",
+//   stdAge: 22,
+//   stdClass: "Web and Mobile",
+//   stdRollNum: 201,
+//   stdSection: "Morning T-T-S",
+// };
+
+
+// console.log("STD1 STD NAME" , std1.stdName )
+// console.log("STD1 STD Age" , std1.stdAge )
+// console.log("STD1 STD section" , std1.stdSection )
+
+
+// var arr= [1,2,3]
+// console.log(typeof arr)
+// console.log(arr)
+
+
+
+
+var std1 = {
+
+    stdName : "Talha",
+    stdAge : 20,
+
+}
+
+///check prop in obj using "in" keyword////
+
+var check = "stdClass" in std1
+
+console.log(check)
+
+// console.log(std1)
+
+///delete prop in obj///
+// delete std1.
+// delete std1.stdAge
+// console.log(std1)
+
+
+
+
+
