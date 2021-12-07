cf6ee5f42afd9948abf9c0b26a5c52b1b17c1829
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2021-12-07 12:36:19 +0500

    7/12/21 object last class

---
+++ class 10/app.js
@@ -0,0 +1,205 @@
+// console.log("hello world")
+
+// var obj = {
+//     name : "Jaffar"
+// }
+
+// console.log(obj.name)
+
+// var obj = {};
+
+// console.log(obj)
+
+// obj.name = "Jaffar"
+
+// console.log(obj)
+
+// var std1 = {
+//     name : "Jaffar",
+//     age : 20,
+//     id : 102,
+//     className : "Web and Mobile"
+// }
+// document.write(std1)
+// console.log(std1.age)
+
+// console.log(std1["age" , "name" , "className"])
+
+// var check = "age" in std1
+
+// console.log(check)
+
+// console.log(std1."name")
+// for( var key in std1 ){
+//     console.log(key , std1[key])
+// }
+
+/////ARRAY IN OBJECT//////
+
+// var std1 = {
+//     name : "Jaffar",
+//     age : 20,
+//     id : 102,
+//     className : "Web and Mobile",
+//     subjects : ["HTML", "CSS" , "JAVASCRIPT"],
+//     marks : [50 , 60 , 40]
+// }
+
+// console.log(std1.subjects[0] , std1.marks[0])
+// console.log(std1.subjects[1] , std1.marks[1])
+// console.log(std1.subjects[2] , std1.marks[2])
+
+
+
+
+// var arr = [  
+//     {
+//         name : "Jaffar"
+//     } , 
+//     {
+//         name : "Umer",
+//         array : [1,2,3]
+//     }     
+// ]
+
+
+// console.log(arr[1].array[2])
+
+
+
+// object in object
+
+
+// var std1 = {
+//     name : "Jaffar",
+//     age : 20,
+//     id : 102,
+//     className : "Web and Mobile",
+//     marks : {
+//         html : 50,
+//         css : 50,
+//         js: 50
+//     }
+// }
+
+
+
+// console.log(std1.marks["html"])
+
+
+// var obj ={
+//     name : "jaffar",
+//     age : 20,
+//     isOnline : true,
+//     result : undefined,
+//     subjects : [],
+
+// }
+
+
+
+
+            // Objects:
+            // Methods
+
+
+
+
+            
+// var std1 = {
+//     name : "Jaffar",
+//     age : 20,
+//     id : 102,
+//     className : "Web and Mobile",
+//     marks : {
+//         html : 50,
+//         css : 50,
+//         js: 80
+//     },
+//     // msg : function(){
+//     //     // console.log("HELLO ")
+//     //     // var a = "std1 object"
+//     //     // return  a
+
+//     // },
+//     obtMarks : function(){
+//         var obt = this.marks.html + this.marks.css + this.marks.js
+//           return obt
+//     },
+//     percentage : function(){
+//         var percentage = (this.obtMarks() / 300) * 100
+//         return percentage
+//     },
+//     getName :function(myName){
+//         return myName
+//     }
+
+
+// }            
+
+
+// console.log(std1.obtMarks())
+// console.log(std1.getName('Ahmed'))
+// console.log("percentage" , std1.percentage())
+
+
+
+
+// var std1 = {
+//     name,
+// }
+// var std2 = {
+//     name,
+// }
+
+
+// Constructors
+
+
+function Std(name , age , id){
+        this.stdName = name
+        this.stdAge = age
+        this.stdId = id
+        this.getName = function(){
+            return this.stdName
+        }
+}
+
+
+// var userName = prompt("name")
+
+// var std1 = new  Std(userName , 20 , 202)
+// var std2 = new  Std("Ahmed" , 23 , 105)
+// var std3 = new  Std("Talha" , 21 , 502)
+// var std4 = new  Std("farhan" , 25 , 101)
+
+
+
+
+// // console.log(std1)
+// console.log(std2)
+// console.log(std3)
+// console.log(std4.hasOwnProperty("stdName"))
+
+
+
+
+<div>
+    <li>
+        asd
+        <edit>
+            <update></update>
+        <edit>
+    </li>
+</div>
+
+
+function  (params) {
+    var edit. diav
+}
+
+
+var li = array
+for (let index = 0; index < array.length; index++) {
+    li[i].2ndchold.disabled = true
+    
